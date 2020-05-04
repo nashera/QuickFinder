e38658da4e6db6817ae2938d197146a3704cdcf8
brobang <nashera@163.com>
2020-05-04 15:26:31 +0800

    go.mod modified

--- go.mod
+++ go.mod
@@ -1,9 +1,3 @@
-module main
+module github.com/nashera/QuickFinder
 
 go 1.14
-
-require (
-	fyne.io/fyne v1.2.4
-	github.com/PuerkitoBio/goquery v1.5.1 // indirect
-	github.com/mattn/go-sqlite3 v2.0.3+incompatible
-)
--- search/search_result.go
+++ search/search_result.go
@@ -6,23 +6,7 @@ import (
 	"time"
 )
 
-// ResultItem 搜索结果
-type ResultItem struct {
-	name       string
-	ResultType string
-	Created    time.Time
-	Modified   time.Time
-	Accessed   time.Time
-	FullPath   string
-}
 
-func createResultItem(info os.FileInfo) *ResultItem {
-	var r ResultItem
-	r = ResultItem{
-		name: info.Name(),
-	}
-	return &r
-}
 
 // func String()
 
